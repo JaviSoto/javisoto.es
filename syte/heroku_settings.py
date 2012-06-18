7c6c025a41315892fec9cf33a5ac1296bb0b2f80
Javier Soto <javier.api@gmail.com>
2012-06-17 21:46:01 -0700

    Added heroku settings file to load them from the environment

---
+++ syte/heroku_settings.py
@@ -0,0 +1,50 @@
+#Blog Integration: Tumblr
+TUMBLR_BLOG_URL = 'javisoto.tumblr.com'
+TUMBLR_API_URL = 'http://api.tumblr.com/v2/blog/{0}'.format(TUMBLR_BLOG_URL)
+TUMBLR_API_KEY = os.environ['TUMBLR_API_KEY']
+
+#RSS Feed Integration: (by default use Tumbrl rss feed)
+RSS_FEED_ENABLED = True
+RSS_FEED_URL = 'http://{0}/rss'.format(TUMBLR_BLOG_URL)
+
+#Twitter Integration
+TWITTER_INTEGRATION_ENABLED = True
+TWITTER_API_URL = 'http://api.twitter.com/1/statuses/user_timeline.json?include_rts=false&exclude_replies=true&count=50&screen_name='
+TWITTER_CONSUMER_KEY = os.environ['TWITTER_CONSUMER_KEY']
+TWITTER_CONSUMER_SECRET = os.environ['TWITTER_CONSUMER_SECRET']
+TWITTER_USER_KEY = os.environ['TWITTER_USER_KEY']
+TWITTER_USER_SECRET = os.environ['TWITTER_USER_SECRET']
+
+
+#Github Integration
+GITHUB_INTEGRATION_ENABLED = True
+GITHUB_API_URL = 'https://api.github.com/'
+GITHUB_ACCESS_TOKEN = os.environ['GITHUB_ACCESS_TOKEN']
+
+GITHUB_OAUTH_ENABLED = False
+GITHUB_CLIENT_ID = os.environ['GITHUB_CLIENT_ID']
+GITHUB_CLIENT_SECRET = os.environ['GITHUB_CLIENT_SECRET']
+GITHUB_OAUTH_AUTHORIZE_URL = 'https://github.com/login/oauth/authorize'
+GITHUB_OAUTH_ACCESS_TOKEN_URL = 'https://github.com/login/oauth/access_token'
+
+
+#Dribbble Integration
+DRIBBBLE_INTEGRATION_ENABLED = False
+DRIBBBLE_API_URL = 'http://api.dribbble.com/players/'
+
+
+#Instagram Integration
+INSTAGRAM_INTEGRATION_ENABLED = True
+INSTAGRAM_API_URL = 'https://api.instagram.com/v1/'
+INSTAGRAM_ACCESS_TOKEN = os.environ['INSTAGRAM_ACCESS_TOKEN']
+INSTAGRAM_USER_ID = os.environ['INSTAGRAM_USER_ID']
+
+INSTAGRAM_OAUTH_ENABLED = False
+INSTAGRAM_CLIENT_ID = os.environ['INSTAGRAM_CLIENT_ID']
+INSTAGRAM_CLIENT_SECRET = os.environ['INSTAGRAM_CLIENT_SECRET']
+INSTAGRAM_OAUTH_AUTHORIZE_URL = 'https://api.instagram.com/oauth/authorize'
+INSTAGRAM_OAUTH_ACCESS_TOKEN_URL = 'https://api.instagram.com/oauth/access_token'
+
+
+#Google Analytics
+GOOGLE_ANALYTICS_TRACKING_ID = os.environ['GOOGLE_ANALYTICS_TRACKING_ID']
