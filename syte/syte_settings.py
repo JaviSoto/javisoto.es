2f45731b480948c09692d250d06cb459ee3569c5
Javier Soto <javier.api@gmail.com>
2012-08-31 11:18:47 -0700

    Fixing redirect loop for realz

--- syte/syte_settings.py
+++ syte/syte_settings.py
@@ -4,11 +4,11 @@ DEPLOYMENT_MODE = 'prod'
 COMPRESS_REVISION_NUMBER = '1.1'
 
 if DEPLOYMENT_MODE == 'dev':
-	SITE_ROOT_URI = 'http://127.0.0.1:8000/'
+	SITE_ROOT_URI = '127.0.0.1:8000'
 	PREPEND_WWW = False
 	DEBUG = True
 else:
-	SITE_ROOT_URI = 'http://www.javiersoto.me'
+	SITE_ROOT_URI = 'www.javiersoto.me'
 	PREPEND_WWW = True
 	DEBUG = False
 
