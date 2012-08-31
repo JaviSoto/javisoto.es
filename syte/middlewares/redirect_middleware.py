e90eb29f39f26d3ccbe758fe7024566589e58d20
Javier Soto <javier.api@gmail.com>
2012-08-31 11:09:02 -0700

    Fixing infinite redirection loop

--- syte/middlewares/redirect_middleware.py
+++ syte/middlewares/redirect_middleware.py
@@ -5,5 +5,5 @@ from syte.syte_settings import *
 
 class ValidateHostMiddleware(object):
 	def process_request(self, request):
-		if DEPLOYMENT_MODE == 'prod' and not request.META['HTTP_HOST'].endswith(SITE_ROOT_URI):
+		if DEPLOYMENT_MODE == 'prod' and not request.META['HTTP_HOST'].startswith(SITE_ROOT_URI):
 			return HttpResponseRedirect(SITE_ROOT_URI)
